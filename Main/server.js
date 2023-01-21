95c6d32e9b1c3c1fd37e2e789ed44ff29df82141
Peter Krysinski <pakrysin@gmail.com>
2023-01-21 10:40:00 -0700

    Updating logic in server.js.  I can finally return data to the screen now for the viewAllEmployees functionality, but for some reason it's displaying as an object, and not the nicely formatted MySQL table query like I'm expecting

--- Main/server.js
+++ Main/server.js
@@ -3,6 +3,17 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: process.env.MYSQL_PASSWORD,
+        database: 'employees_db'
+    },
+    console.log(`Connected to the employees_db database.`)
+);    
+
 // User Inquirer to prompt user (in terminal via NodeJS) for answers based on questions about subjects in the standard HTML file template.
 const trackEmployees = () => {
 
@@ -25,24 +36,31 @@ const trackEmployees = () => {
             case 'View All Employees':
                 console.log(response.startList);
                 viewAllEmployees();
+                trackEmployees();
                 break;
             case 'Add Employee':
                 console.log(response.startList);
+                trackEmployees();
                 break;
             case 'Update Employee Role':
                 console.log(response.startList);
+                trackEmployees();
                 break;
             case 'View All Roles':
                 console.log(response.startList);
+                trackEmployees();
                 break;
             case 'Add Role':
                 console.log(response.startList);
+                trackEmployees();
                 break;
             case 'View All Departments':
                 console.log(response.startList);
+                trackEmployees();
                 break;               
             case 'Add Department':
                 console.log(response.startList);
+                trackEmployees();
                 break;       
             case 'Quit':
                 console.log("Goodbye!");
@@ -55,31 +73,17 @@ const trackEmployees = () => {
 
 const viewAllEmployees = () => {
     const sql = `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name, role.salary, manager_id FROM department JOIN role ON department.id = role.department_id JOIN employee ON role.id = employee.role_id;`;
-  
-    // Connect to database
-    const db = mysql.createConnection(
-        {
-          host: 'localhost',
-          user: 'root',
-          password: process.env.MYSQL_PASSWORD,
-          database: 'employees_db'
-        },
-        console.log(`Connected to the employees_db database.`)
-    );    
 
-    console.log("PETER1");
-    db.query(sql, (err, rows) => {
-      if (err) {
-         console.log({ error: err.message });
-         return;
-      }
-      return {
-        message: 'success',
-        data: rows
-      };
-    });    
-    console.log("PETER2");
-}
+    db.query(sql, function(err, results) {
+        if (err) {
+            console.log({ error: err.message });
+            return;
+         } else {
+            console.log(results); // results contains rows returned by server   
+         }
+        }
+      );   
+};
 
 // Initialize the program and start building your team!
 trackEmployees();
